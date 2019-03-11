befad501e8cdcfcc8f4d2eebb0f44ed6f2b30bad
Raj7839 <rajgopal.singh03@gmail.com>
2019-03-11 23:33:22 +0530

    youlike v2

---
+++ build/precache-manifest.572b3f5e54842038c7681d5a221c7315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "792d7d9396d3463e8d20",
+    "url": "/youtubelike/static/js/runtime~main.2a5747b0.js"
+  },
+  {
+    "revision": "82aecd88850662f38a02",
+    "url": "/youtubelike/static/js/main.4717c0e9.chunk.js"
+  },
+  {
+    "revision": "5d21101413ea9baee411",
+    "url": "/youtubelike/static/js/2.589c286c.chunk.js"
+  },
+  {
+    "revision": "82aecd88850662f38a02",
+    "url": "/youtubelike/static/css/main.6a82981d.chunk.css"
+  },
+  {
+    "revision": "c12818efda962f60f35d3499c4e6f3ef",
+    "url": "/youtubelike/index.html"
+  }
+];
